dd7d03d3d483700a46451f784293209d100e94b5
Alex Krantz <alex@krantz.dev>
2021-03-30 12:51:31 -0700

    Generate readable content for slack and discord webhooks

--- app/lib/webhooks.rb
+++ app/lib/webhooks.rb
@@ -46,19 +46,23 @@ module Webhooks
     end
 
     def discord(url, type, **data)
+      # Generate the human readable content for the message
+      description, fields = generate_content(type, **data)
+
       client = Discordrb::Webhooks::Client.new(url: url)
       response = client.execute do |builder|
         builder.add_embed do |embed|
           # Set the constants
           embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: HackathonConfig['name'], icon_url: HackathonConfig['logo_asset'])
-          embed.color = '#1c7ed6' # TODO: add hackathon specific color if exists
+          embed.color = '#1c7ed6'
           embed.timestamp = Time.now
-
-          # TODO: figure out how to format data based on event type
 
           # Set the title and content
           embed.title = READABLE_EVENTS[type]
-          embed.description = "<PLACEHOLDER>"
+          embed.description = description
+
+          # Set the fields
+          fields.each { |field| embed.add_field(**field, inline: true) }
         end
       end
 
@@ -66,16 +70,17 @@ module Webhooks
     end
 
     def slack(url, type, **data)
+      # Generate the human readable content for the message
+      description, fields = generate_content(type, **data)
+
       # Parse the URI
       uri = URI.parse(url)
 
       # Build the request
       req = Net::HTTP::Post.new(uri, { 'Content-Type' => 'application/json' })
 
-      # TODO: figure out how to format data based on event type
-
       # Setup the request body as an attachment
-      req.body = {
+      body = {
         text: "#{type}: #{data.to_json}",
         blocks: [
           {
@@ -92,12 +97,24 @@ module Webhooks
             fields: [
               {
                 type: "plain_text",
-                text: "Testing"
+                text: description
               }
             ]
           }
         ]
-      }.to_json
+      }
+
+      # Add fields if necessary
+      unless fields.empty?
+        body[:blocks] << {
+          type: "section",
+          block_id: "fields",
+          fields: fields.map { |field| { type: "mrkdwn", text: "*#{field[:name]}*: #{field[:value]}" } }
+        }
+      end
+
+      # Add the body to the request
+      req.body = body.to_json
 
       # Send the request
       response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
@@ -105,6 +122,55 @@ module Webhooks
       end
 
       [response.code.to_i, response.body]
+    end
+
+    def generate_content(type, **data)
+      case type
+      when "questionnaire.pending"
+        questionnaire = data[:questionnaire]
+        school = questionnaire.school
+        user = questionnaire.user
+        [
+          "_#{user.first_name} #{user.last_name}_ (id: #{user.id}) just applied!",
+          [
+            {
+              name: "School",
+              value: school.full_name.present? ? school.full_name : "<not provided>"
+            },
+            {
+              name: "Discord",
+              value: questionnaire.discord
+            }
+          ]
+        ]
+      when "questionnaire.late_waitlist"
+        questionnaire = data[:questionnaire]
+        school = questionnaire.school
+        user = questionnaire.user
+        [
+          "_#{user.first_name} #{user.last_name}_ (id: #{user.id}) just applied! They were automatically waitlisted since the deadline passed.",
+          [
+            {
+              name: "School",
+              value: school.full_name.present? ? school.full_name : "<not provided>"
+            },
+            {
+              name: "Discord",
+              value: questionnaire.discord
+            }
+          ]
+        ]
+      when "questionnaire.rsvp_confirmed"
+        questionnaire = data[:questionnaire]
+        user = questionnaire.user
+        ["_#{user.first_name} #{user.last_name}_ (id: #{user.id}) will be attending!", []]
+      when "questionnaire.rsvp_denied"
+        questionnaire = data[:questionnaire]
+        user = questionnaire.user
+        ["_#{user.first_name} #{user.last_name}_ (id: #{user.id}) will not be attending.", []]
+      when "testing" then ["Just testing that webhooks work.", []]
+      else ["Invalid event, something went very wrong", []]
+      end
     end
   end
 end
